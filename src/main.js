2a5a270cb4d1d873dad85640733eaf95fc431937
liangshan <loveliangshan@126.com>
2019-07-05 18:44:14 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 // import router from './router'
-import Framework7 from 'framework7'
-import Framework7Vue from 'framework7-vue'
-import 'framework7/css/framework7.bundle.min.css'
+// Import Framework7
+import Framework7 from 'framework7/framework7.esm.bundle.js';
+
+// Import Framework7-Vue Plugin
+import Framework7Vue from 'framework7-vue/framework7-vue.esm.bundle.js';
+
+// Import Framework7 Styles
+import 'framework7/css/framework7.bundle.css';
+
 import 'framework7-icons'
 
 Framework7.use(Framework7Vue)
