ecfee214ffd1fd78a6dfc8e9bef1c5feb007148a
liangshan <loveliangshan@126.com>
2019-07-09 19:05:49 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,10 @@
       <f7-statusbar></f7-statusbar>
       <f7-panel left
                 reveal
-                class="theme-dark">
+                class=" panel-reveal">
         <f7-view>
           <f7-page>
-            <f7-navbar title="Left Panel"></f7-navbar>
+            <!-- <f7-navbar title="Left Panel"></f7-navbar> -->
             <f7-block>
               <p>Here comes the left panel text</p>
             </f7-block>
@@ -65,7 +65,13 @@
           routes,
           theme: 'ios',
           name: 'Articles',
-          id: 'com.dei2.articles'
+          id: 'com.dei2.articles',
+          // panel: {
+          //   swipe: 'both',
+          // },
+          panels3d: {
+            enabled: true
+          }
         }
       }
     }
@@ -73,6 +79,39 @@
 </script>
 
 <style>
+  body {
+    background: #074d55;
+    background: -moz-radial-gradient(
+      center,
+      ellipse cover,
+      #a7cfdf 0%,
+      #23538a 100%
+    ); /* FF3.6+ */
+    background: -webkit-gradient(
+      radial,
+      right bottom,
+      0px,
+      left top,
+      100%,
+      color-stop(0%, #6967a5),
+      color-stop(50%, #2075a1),
+      color-stop(100%, #074d55)
+    );
+    background: -webkit-radial-gradient(
+      right bottom,
+      ellipse cover,
+      #6967a5 0%,
+      #2075a1 50%,
+      #074d55 100%
+    );
+    background: radial-gradient(
+      ellipse at right bottom,
+      rgba(10, 81, 15, 0.6) 0%,
+      rgba(10, 81, 15, 0.9) 50%,
+      rgba(10, 81, 15, 1) 100%
+    );
+  }
+  /* #6967a5 0%, #2075a1 50%, #074d55 100% */
   .card-content-padding {
     font-size: 16px;
   }
@@ -81,5 +120,27 @@
   }
   .card-content-padding img {
     max-width: 100%;
+  }
+
+  .panel .page,
+  .panel {
+    background: none;
+  }
+  .panel .list ul {
+    background: none;
+    border: none;
+  }
+  .panel .list .item-link {
+    color: #fff;
+  }
+  .panel .list .item-link.active-state {
+    background: rgba(255, 255, 255, 0.2);
+  }
+  .panel .list .item-inner {
+    border-bottom-color: rgba(255, 255, 255, 0.2);
+  }
+  .panel .block,
+  .panel .block-title {
+    color: #fff;
   }
 </style>
