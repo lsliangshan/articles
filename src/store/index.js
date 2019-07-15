1a2fb02818e3262054e2239ec9d8dda2d16f2ef6
梁山 <liangshan@192.168.0.108>
2019-07-16 00:45:10 +0800

    no message

--- src/store/index.js
+++ src/store/index.js
@@ -122,8 +122,8 @@ const store = new Vuex.Store({
       }
     },
     requestInfo: {
-      baseUrl: 'http://127.0.0.1:3000',
-      // baseUrl: 'https://talkapi.dei2.com',
+      // baseUrl: 'http://127.0.0.1:3000',
+      baseUrl: 'https://talkapi.dei2.com',
       login: '/Zpm/user/login',
       register: '/Zpm/user/register',
       articles: {
